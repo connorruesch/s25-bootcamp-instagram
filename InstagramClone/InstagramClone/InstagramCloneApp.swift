7715d9306a5b8f67e23b26652da09d1d784d9e17
connorruesch <connorruesch@outlook.com>
2025-02-04 19:30:58 -0500

    initial commit

---
+++ InstagramClone/InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Connor Ruesch on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InstagramClone/InstagramCloneTests/InstagramCloneTests.swift
@@ -0,0 +1,16 @@
+//
+//  InstagramCloneTests.swift
+//  InstagramCloneTests
+//
+//  Created by Connor Ruesch on 2/4/25.
+//
+
+import Testing
+
+struct InstagramCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
